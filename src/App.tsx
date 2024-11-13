57a4fca3ff0b67813c8fbf30330cb3fbca794440
Fruvvien <hunfantasztik@gmail.com>
2024-11-13 15:49:10 +0100

    ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import Greeting from "./Greeting";
 
 function App(){
-  const [count, setCount] = useState(0)
+ /*  const [user, setUser] = useState({name:"", age: ""}); */
+  const [tasks, setTasks] = useState([]);
+  const [newTasks, setNewTaks] = useState([]);
+  const addTask = () => {
+    if(newTasks.trim() === ""){
+      return;
+    } 
+    setTasks((prevTasks)=> [...prevTasks, newTasks]);
+
+    setNewTaks(""){
+
+    }
+  }
+  /* const updateName = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setUser((prevUser)=> ({...prevUser, name: e.target.value }) )
+  }
+  const updateAge = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setUser((prevUser)=> ({...prevUser, age: e.target.value }) )
+  } */
+
+
   return (
+
   <>
+   
     <div>
-      <h1>Hello, React with Vite</h1>
-      <p>This is my first React component.</p>
-      <Greeting name="ASd" age={2}/>
-      <h2>Counter: {count}</h2>
-      <button onClick={() =>  setCount(count + 1)}>Press</button>
+      <h2>Feldadatlista</h2>
+      <input type="text" name="" id="" />
+
+
+     {/*  <h2>Felhasználói adatok</h2>
+      Név: <input type="text" value={user.name} onChange={updateName} /><br />
+      Age: <input type="text" value={user.age} onChange={updateAge} />
+      <p>Név: {user.name}</p>
+      <p>Age: {user.age}</p> */}
+
+      
     </div>
   
   </>
