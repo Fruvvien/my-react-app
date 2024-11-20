bbaa5a68a04c4b49c9f1da3b4fd93eafa22eb943
Fruvvien <hunfantasztik@gmail.com>
2024-11-20 13:54:14 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, SetStateAction} from "react";
 import Greeting from "./Greeting";
+
+type Task = {name: string, completed: boolean}
+
 
 function App(){
  /*  const [user, setUser] = useState({name:"", age: ""}); */
-  const [tasks, setTasks] = useState([]);
-  const [newTasks, setNewTaks] = useState([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskName, setNewTakName] = useState('');
   const addTask = () => {
-    if(newTasks.trim() === ""){
-      return;
-    } 
-    setTasks((prevTasks)=> [...prevTasks, newTasks]);
+    if(newTaskName.trim() === ''){return } 
+    const newTask: Task = {name: newTaskName, completed: false};
 
-    setNewTaks(""){
 
-    }
+    setTasks((prevTasks : any)=> [...prevTasks, newTaskName]);
+    setNewTakName('');
   }
+
+  const removeTask = (indexToRemove : number) =>{
+    setTasks((prevTasks)=> prevTasks.filter((task, index) => index !== indexToRemove));
+  };
+
+
+  const toggleTaskCompletion = (index: number) =>{
+    setTasks((prevTasks) => prevTasks.map((task, i) => i === index ? {...task, completed: !task.completed}: task)
+  )}
+
+
+}
+
+
   /* const updateName = (e: React.ChangeEvent<HTMLInputElement>) =>{
     setUser((prevUser)=> ({...prevUser, name: e.target.value }) )
   }
@@ -26,11 +41,27 @@ function App(){
   return (
 
   <>
-   
+
+
+
     <div>
       <h2>Feldadatlista</h2>
-      <input type="text" name="" id="" />
-
+      <input type="text" value={newTaskName} onChange={(e) =>{
+        setNewTakName(e.target.value)
+      }} name="" id="" />
+      <button onClick={addTask}>Hozzáadás</button>
+      <ul>
+        {
+          tasks.map((task,index) =>(
+            <li key={index}>{task}</li>
+          ))
+          
+        }
+        {
+          
+          <button onClick={removeTask()}></button>
+        }
+      </ul>
 
      {/*  <h2>Felhasználói adatok</h2>
       Név: <input type="text" value={user.name} onChange={updateName} /><br />
